b1e9313e1df09bbd2f1a1455db161b34e05f3200
Carlos David Mendoza Archaga <carlosdmendoza99@gmail.com>
2023-10-03 01:09:16 -0600

    Added Event Items Form

--- script.js
+++ script.js
@@ -15,22 +15,7 @@ function copyApexCode() {
     alert('Code copied to clipboard!');
 }
 
-// This will copy the code for Event Items Creation.
-function copyApexCode2() {
-    const apexCodeElement = document.getElementById('apexCode2');
-    const apexCode = apexCodeElement.innerText;
 
-    const textarea = document.createElement('textarea');
-    textarea.value = apexCode;
-    document.body.appendChild(textarea);
-
-    textarea.select();
-    document.execCommand('copy');
-
-    document.body.removeChild(textarea);
-
-    alert('Code copied to clipboard!');
-}
 
 document.getElementById('snippetForm').addEventListener('submit', function(event) {
     event.preventDefault();
